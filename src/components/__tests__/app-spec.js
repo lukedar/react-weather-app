360bb7ffd61e5d51218fb6b8adfba1837407567a
luke.darham <luke.darham@gmail.com>
2017-03-06 14:52:34 +0000

    Add skeleton test for app.js

---
+++ src/components/__tests__/app-spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+describe('App', () => {
+  beforeEach(() => {
+  });
+
+  afterEach(() => {
+  });
+
+  describe('Render', () =>  {
+    it('should render a searchBar and WeatherCard component', () =>  {
+			// let searchBar = TestUtils.findRenderedComponentWithType(App, searchBar);
+			// let weatherCard = TestUtils.findRenderedComponentWithType(App, searchBar);
+	  	//  expect(searchBar).toEqual(1);
+	    });
+  });
+});
